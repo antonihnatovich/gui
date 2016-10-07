23d0bc3bf7945c49b5c172c46726c769388e3d24
Hello Wordovic <tf.2.98@mail.ru>
2016-10-07 05:23:51 +0300

    before

--- gui/app/src/main/java/com/l/gui/MainActivity.java
+++ gui/app/src/main/java/com/l/gui/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -31,5 +32,6 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setSubtitle("online");
         actionBar.setTitle("Антон Игнатович");
+
     }
 }
